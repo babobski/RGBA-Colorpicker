0da62e3715f262b63e6d538b0d8b6e8bcd44c13b
babobski <info@babobski.nl>
2016-11-07 08:19:40 +0100

    updated ui and added save memory colors function

--- bootstrap.js
+++ bootstrap.js
@@ -2,37 +2,44 @@ const { classes: Cc, interfaces: Ci, utils: Cu, manager: Cm } = Components;
 Cu.import('resource://gre/modules/Services.jsm');
 
 function startup(data, reason) {
-    
-    if ( ! ("@babobski.com/rgbaColorPicker;1" in Cc)) {
-        try {
-            var component = data.installPath.clone();
-            component.append("components");
-            component.append("component.manifest");
-    
-            var registrar = Cm.QueryInterface(Ci.nsIComponentRegistrar);
-            registrar.autoRegister(component);
-        } catch (e) {
-            Cu.reportError("Commando: Exception while registering component for Colorpicker");
-            Cu.reportError(e);
-        }
-    }
-    
+	
+	if ( ! ("@babobski.com/rgbaColorPicker;1" in Cc)) {
+		try {
+			var component = data.installPath.clone();
+			component.append("components");
+			component.append("component.manifest");
+	
+			var registrar = Cm.QueryInterface(Ci.nsIComponentRegistrar);
+			registrar.autoRegister(component);
+		} catch (e) {
+			Cu.reportError("Commando: Exception while registering component for Colorpicker");
+			Cu.reportError(e);
+		}
+	}
+	
 };
 
 function shutdown(data, reason) {};
 
 function install(data, reason) {
-    let windows = Services.wm.getEnumerator("Komodo");
-    let domWindow = windows.getNext().QueryInterface(Ci.nsIDOMWindow);
-    
-    var prefs = domWindow.require("ko/prefs");
-    prefs.setString('colorpicker_cid', '@babobski.com/rgbaColorPicker;1');
+	let windows = Services.wm.getEnumerator("Komodo");
+	let domWindow = windows.getNext().QueryInterface(Ci.nsIDOMWindow);
+	
+	var prefs = domWindow.require("ko/prefs");
+	var koLess = domWindow.require("ko/less");
+	var defaultColors = '[{"r":17,"g":17,"b":17,"a":1},{"r":255,"g":255,"b":255,"a":1},{"r":170,"g":170,"b":170,"a":1},{"r":221,"g":221,"b":221,"a":1},{"r":46,"g":204,"b":64,"a":1},{"r":255,"g":65,"b":54,"a":1},{"r":255,"g":220,"b":0,"a":1},{"r":0,"g":116,"b":217,"a":1}]';
+	prefs.setString('colorpicker_cid', '@babobski.com/rgbaColorPicker;1');
+	prefs.setString('colorpicker.savedColors', defaultColors);
+	koLess.loadSheet('less://rgba-colorpicker/skin/colorpicker.less');
+	koLess.reload(true);
 };
 
 function uninstall(data, reason) {
-    let windows = Services.wm.getEnumerator("Komodo");
-    let domWindow = windows.getNext().QueryInterface(Ci.nsIDOMWindow);
-    
-    var prefs = domWindow.require("ko/prefs");
-    prefs.deletePref('colorpicker_cid');
+	let windows = Services.wm.getEnumerator("Komodo");
+	let domWindow = windows.getNext().QueryInterface(Ci.nsIDOMWindow);
+	
+	var prefs = domWindow.require("ko/prefs");
+	prefs.deletePref('colorpicker_cid');
+	prefs.deletePref('colorpicker.savedColors');
 };
+
--- components/component.js
+++ components/component.js
@@ -1,5 +1,5 @@
 /* Copyright (c) 2009 ActiveState
-   See the file LICENSE.txt for licensing information. */
+	 See the file LICENSE.txt for licensing information. */
 
 /**
  * This is a simple XPCOM wrapper around John Dyers excellent JavaScript
@@ -12,7 +12,7 @@ const Ci = Components.interfaces;
 Components.utils.import("resource://gre/modules/XPCOMUtils.jsm");
 
 /***********************************************************
- *              XPCOM class definition                     *
+ *				XPCOM class definition					 *
  ***********************************************************/
 
 // Class constructor.
@@ -20,138 +20,147 @@ function rgbaColorPicker() {}
 
 // Class definition.
 rgbaColorPicker.prototype = {
-    
-    // properties required for XPCOM registration:
-    classDescription: "RGBA Colorpicker",
-    
-    classID:          Components.ID("{ab5e2112-d8ac-4794-8145-b3c4a5cd5ab7}"),
+	
+	// properties required for XPCOM registration:
+	classDescription: "RGBA Colorpicker",
+	
+	classID:			Components.ID("{de710bf1-b4b6-4ef5-ac8a-beb4ea958e81}"),
 
-    QueryInterface: XPCOMUtils.generateQI([Ci.koIColorPicker,
-                                           Ci.koIColorPickerAsync]),
+	QueryInterface: XPCOMUtils.generateQI([Ci.koIColorPicker,
+											 Ci.koIColorPickerAsync]),
 
-    chromeURL: "chrome://rgba-colorpicker/content/wrapper.xul",
+	chromeURL: "chrome://rgba-colorpicker/content/wrapper.xul",
 
-    /**
-     * Select a color from the Komodo color picker dialog.
-     * @deprecated since Komodo 7.0b1 - use pickColorAsync instead
-     * 
-     * @param {string} hexColor - The initial color.
-     */
-    pickColor: function(hexColor) {
-        return this.pickColorWithPositioning(hexColor, -1, -1);
-    },
+	/**
+	 * Select a color from the Komodo color picker dialog.
+	 * @deprecated since Komodo 7.0b1 - use pickColorAsync instead
+	 * 
+	 * @param {string} hexColor - The initial color.
+	 */
+	pickColor: function(hexColor) {
+		return this.pickColorWithPositioning(hexColor, -1, -1);
+	},
 
-    /**
-     * Select a color from the Komodo color picker dialog, attempting to
-     * position the dialog at the given coordinates.
-     *
-     * @param   {String} hexColor - The initial color, as "#nnnnnn".
-     * @param   {Number} screenX  - The initial X position, or null.
-     * @param   {Number} screenY  - The initial Y position, or nul.
-     *
-     * @returns {String} The picked color, as "#nnnnnn", or null.
-     */
-    pickColorWithPositioning: function(hexColor, screenX, screenY) {
-        /**
-         * @type {Components.interfaces.koIPreferenceSet}
-         */
-        var prefs = Components.classes["@activestate.com/koPrefService;1"].
-                        getService(Components.interfaces.koIPrefService).prefs;
-        var colorMode = prefs.getString("colorpicker.colorMode", "h");
+	/**
+	 * Select a color from the Komodo color picker dialog, attempting to
+	 * position the dialog at the given coordinates.
+	 *
+	 * @param	 {String} hexColor - The initial color, as "#nnnnnn".
+	 * @param	 {Number} screenX	- The initial X position, or null.
+	 * @param	 {Number} screenY	- The initial Y position, or nul.
+	 *
+	 * @returns {String} The picked color, as "#nnnnnn", or null.
+	 */
+	pickColorWithPositioning: function(hexColor, screenX, screenY) {
+		/**
+		 * @type {Components.interfaces.koIPreferenceSet}
+		 */
+		var prefs = Components.classes["@activestate.com/koPrefService;1"].
+						getService(Components.interfaces.koIPrefService).prefs;
+		var colorMode = prefs.getString("colorpicker.colorMode", "h");
+		var savedColors = prefs.getString('colorpicker.savedColors');
 
-        var args = {
-            hexColor: hexColor,
-            colorMode: colorMode,
-            retval: 0
-        };
-        args.wrappedJSObject = args;
+		var args = {
+			hexColor: hexColor,
+			colorMode: colorMode,
+			savedColors: savedColors,
+			retval: 0
+		};
+		args.wrappedJSObject = args;
 
-        var win = Components.classes['@mozilla.org/appshell/window-mediator;1']
-                      .getService(Components.interfaces.nsIWindowMediator)
-                      .getMostRecentWindow(null);
-        var windowFeatures = 'modal,,resizable=no';
-        if (screenX >= 0)
-            windowFeatures += ",left=" + screenX;
-        if (screenY >= 0)
-            windowFeatures += ",top=" + screenY;
-        win.openDialog(this.chromeURL, 'Color Picker',
-                       windowFeatures, args);
+		var win = Components.classes['@mozilla.org/appshell/window-mediator;1']
+						.getService(Components.interfaces.nsIWindowMediator)
+						.getMostRecentWindow(null);
+		var windowFeatures = 'modal,resizable=no';
+		
+		if (screenX >= 0)
+			windowFeatures += ",left=" + screenX;
+		if (screenY >= 0)
+			windowFeatures += ",top=" + screenY;
+		win.openDialog(this.chromeURL, 'Color Picker',
+						 windowFeatures, args);
 
-        if (args.retval) {
-            // Remember the last color mode used, for next time.
-            prefs.setStringPref("colorpicker.colorMode", args.colorMode);
-            return args.hexColor;
-        }
-        return null;
-    },
+		if (args.retval) {
+			// Remember the last color mode used, for next time.
+			prefs.setStringPref("colorpicker.colorMode", args.colorMode);
+			return args.hexColor;
+		}
+		return null;
+	},
 
-    /**
-     * Select a color from the Komodo color picker dialog.  The picking may be
-     * asynchronous.
-     *
-     * @param   {koIColorPickerAsyncCallback} aCallback - the callback to invoke
-     *              When the color has been chosen.
-     * @param   {String} aStartingColor - The initial color, as a hex string of
-     *              #rrggbb.
-     * @param   {double} aStartingAlpha - The initial alpha component, as a
-     *              number between 0.0 and 1.0 inclusive.
-     * @param   {Number} aScreenX - The initial dialog X position, or null.
-     * @param   {Number} aScreenY - The initial dialog Y position, or null.
-     */
-    pickColorAsync: function koColorPicker_pickColorAsync(aCallback,
-                                                          aStartingColor,
-                                                          aStartingAlpha,
-                                                          aScreenX, aScreenY)
-    {
-        if (!aCallback || !(aCallback instanceof Ci.koIColorPickerAsyncCallback)) {
-            throw Components.results.NS_ERROR_INVALID_ARG;
-        }
+	/**
+	 * Select a color from the Komodo color picker dialog.	The picking may be
+	 * asynchronous.
+	 *
+	 * @param	 {koIColorPickerAsyncCallback} aCallback - the callback to invoke
+	 *				When the color has been chosen.
+	 * @param	 {String} aStartingColor - The initial color, as a hex string of
+	 *				#rrggbb.
+	 * @param	 {double} aStartingAlpha - The initial alpha component, as a
+	 *				number between 0.0 and 1.0 inclusive.
+	 * @param	 {Number} aScreenX - The initial dialog X position, or null.
+	 * @param	 {Number} aScreenY - The initial dialog Y position, or null.
+	 */
+	pickColorAsync: function koColorPicker_pickColorAsync(aCallback,
+															aStartingColor,
+															aStartingAlpha,
+															aScreenX, aScreenY)
+	{
+		if (!aCallback || !(aCallback instanceof Ci.koIColorPickerAsyncCallback)) {
+			throw Components.results.NS_ERROR_INVALID_ARG;
+		}
 		
 		
-        var colorString = aStartingColor.replace(/^#/, "");
-        if (!/^[0-9a-f]{6}$/i.test(colorString)) {
-            throw Components.results.NS_ERROR_INVALID_ARG;
-        }
-        /**
-         * @type {Components.interfaces.koIPreferenceSet}
-         */
-        var prefs = Components.classes["@activestate.com/koPrefService;1"].
-                        getService(Components.interfaces.koIPrefService).prefs;
-        var colorMode = prefs.getString("colorpicker.colorMode", "h");
+		var colorString = aStartingColor.replace(/^#/, "");
+		if (!/^[0-9a-f]{6}$/i.test(colorString)) {
+			throw Components.results.NS_ERROR_INVALID_ARG;
+		}
+		/**
+		 * @type {Components.interfaces.koIPreferenceSet}
+		 */
+		var prefs = Components.classes["@activestate.com/koPrefService;1"].
+						getService(Components.interfaces.koIPrefService).prefs;
+		var colorMode = prefs.getString("colorpicker.colorMode", "h");
+		var savedColors = prefs.getString('colorpicker.savedColors');
 
-        var args = {
-            hexColor: colorString,
-            colorMode: colorMode,
+		var args = {
+			hexColor: colorString,
+			colorMode: colorMode,
 			opacity: aStartingAlpha,
-            retval: 0
-        };
-        args.wrappedJSObject = args;
+			savedColors: savedColors,
+			retval: 0
+		};
+		args.wrappedJSObject = args;
 
-        var win = Components.classes['@mozilla.org/appshell/window-mediator;1']
-                      .getService(Components.interfaces.nsIWindowMediator)
-                      .getMostRecentWindow(null);
-        var windowFeatures = 'modal,resizable=no';
-        if (aScreenX !== 0 || aScreenY !== 0) {
-            windowFeatures += ",left=" + aScreenX + ",top=" + aScreenY;
-        }
-        win.openDialog(this.chromeURL, 'Color Picker',
-                       windowFeatures, args);
+		var win = Components.classes['@mozilla.org/appshell/window-mediator;1']
+						.getService(Components.interfaces.nsIWindowMediator)
+						.getMostRecentWindow(null);
+		var windowFeatures = 'modal,resizable=no';
+		if (aScreenX !== 0 || aScreenY !== 0) {
+			windowFeatures += ",left=" + aScreenX + ",top=" + aScreenY;
+		}
+		
+		win.openDialog(this.chromeURL, 'Color Picker',
+						 windowFeatures, args);
 
-        if (args.retval) {
-            // Remember the last color mode used, for next time.
-            prefs.setStringPref("colorpicker.colorMode", args.colorMode);
-            colorString = "#" + args.hexColor.replace(/^#/, "");
+		if (args.retval) {
+			// Remember the last color mode used, for next time.
+			prefs.setStringPref("colorpicker.colorMode", args.colorMode);
+			prefs.setString("colorpicker.savedColors", args.savedColors);
+			colorString = "#" + args.hexColor.replace(/^#/, "");
 			colorAlpha = args.opacity;
-            aCallback.handleResult(colorString, colorAlpha);
-        } else {
-            aCallback.handleResult(null, aStartingAlpha);
-        }
-    },
+			aCallback.handleResult(colorString, colorAlpha);
+		} else {
+			aCallback.handleResult(null, aStartingAlpha);
+		}
+	},
 };
 
 // XPCOM registration of class.
 if ("generateNSGetFactory" in XPCOMUtils) {
-    var NSGetFactory = XPCOMUtils.generateNSGetFactory([rgbaColorPicker]);
+	var NSGetFactory = XPCOMUtils.generateNSGetFactory([rgbaColorPicker]);
 } else if ("generateNSGetModule" in XPCOMUtils) {
-    var NSGetModule = XPCOMUtils.generateNSGetModule([rgbaColorPicker]);
+	var NSGetModule = XPCOMUtils.generateNSGetModule([rgbaColorPicker]);
 }
+
+
