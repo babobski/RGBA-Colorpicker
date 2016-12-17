abb46855d9956a550da58d0571091344ba615485
babobski <info@babobski.nl>
2016-12-17 19:56:22 +0100

    added ability to save memory colors updated readme

--- README.md
+++ README.md
@@ -1,10 +1,17 @@
-# RGBA Colorpicker
+# RGBA ColorPicker
 komodo edit/ide
 
-Replaces the default colorpicker with a RGBA compatible Colorpicker
+Replaces the default colorpicker with a RGBA compatible ColorPicker
 
 ## Screenshot
 ![screenshot](screenshot01.png)
+
+## Saving Colors
+When you want to save a color to the ColorPicker memory colors.
+You will have to click on the chosen color and it will be added to the memory colors.
+If you added a memory color and click on cancel the memory color will not be saved.
+
+![memory-colors](memory-colors.gif)
 
 ## Copy, paste and cut
 Copy paste and cut is added in the 1.1 release.  
--- bootstrap.js
+++ bootstrap.js
@@ -26,12 +26,19 @@ function install(data, reason) {
 	let domWindow = windows.getNext().QueryInterface(Ci.nsIDOMWindow);
 	
 	var prefs = domWindow.require("ko/prefs");
-	var koLess = domWindow.require("ko/less");
 	var defaultColors = '[{"r":17,"g":17,"b":17,"a":1},{"r":255,"g":255,"b":255,"a":1},{"r":170,"g":170,"b":170,"a":1},{"r":221,"g":221,"b":221,"a":1},{"r":46,"g":204,"b":64,"a":1},{"r":255,"g":65,"b":54,"a":1},{"r":255,"g":220,"b":0,"a":1},{"r":0,"g":116,"b":217,"a":1}]';
 	prefs.setString('colorpicker_cid', '@babobski.com/rgbaColorPicker;1');
 	prefs.setString('colorpicker.savedColors', defaultColors);
-	koLess.loadSheet('less://rgba-colorpicker/skin/colorpicker.less');
-	koLess.reload(true);
+	
+	// Restart on upgrade to apply changes in style and component
+	if (reason === ADDON_UPGRADE) {
+		Components.classes["@mozilla.org/toolkit/app-startup;1"]
+		.getService(Components.interfaces.nsIAppStartup)
+		.quit(
+		Components.interfaces.nsIAppStartup.eRestart |
+		Components.interfaces.nsIAppStartup.eAttemptQuit
+		);
+	}
 };
 
 function uninstall(data, reason) {
